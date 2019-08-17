cf24fc2d64a5a45a58c840a13a11da44daaa72eb
Gorawade <Ravi.Gorawade@fiserv.co.in>
2019-08-17 12:14:17 +0530

    First Commit

---
+++ src/main/java/com/rsg/cloud/springcloud/SpringCloudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.rsg.cloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTutorialApplication.class, args);
+	}
+
+}
